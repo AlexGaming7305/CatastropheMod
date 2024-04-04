5a4efc4b3be0f87a96c71aaea6c70d472094b95f
AlexGaming7305 <Owner@DESKTOP-QQAGUGK.mynetworksettings.com>
2024-04-03 21:59:39 -0400

    - Duplicated the SpearPinPoint Procedure located on the outside of all the folders

---
+++ src/main/java/net/mcreator/catastrophemod/procedures/SpearPunctureProcedureProcedure.java
@@ -0,0 +1,109 @@
+package net.mcreator.catastrophemod.procedures;
+
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.BlockPos;
+
+import net.mcreator.catastrophemod.network.CatastropheModModVariables;
+import net.mcreator.catastrophemod.init.CatastropheModModEnchantments;
+import net.mcreator.catastrophemod.CatastropheModMod;
+
+import javax.annotation.Nullable;
+
+import java.util.List;
+import java.util.Comparator;
+
+@Mod.EventBusSubscriber
+public class SpearPunctureProcedureProcedure {
+	@SubscribeEvent
+	public static void onEntityAttacked(LivingHurtEvent event) {
+		if (event != null && event.getEntity() != null) {
+			execute(event, event.getEntity().level(), event.getEntity().getX(), event.getEntity().getY(), event.getEntity().getZ(), event.getSource().getEntity(), event.getAmount());
+		}
+	}
+
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity sourceentity, double amount) {
+		execute(null, world, x, y, z, sourceentity, amount);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity sourceentity, double amount) {
+		if (sourceentity == null)
+			return;
+		double pin_point_distance = 0;
+		double damage = 0;
+		if ((sourceentity.getCapability(CatastropheModModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new CatastropheModModVariables.PlayerVariables())).spear_pin_point_strike_cooldown == false) {
+			{
+				boolean _setval = true;
+				sourceentity.getCapability(CatastropheModModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+					capability.spear_pin_point_strike_cooldown = _setval;
+					capability.syncPlayerVariables(sourceentity);
+				});
+			}
+			CatastropheModMod.queueServerWork(15, () -> {
+				{
+					boolean _setval = false;
+					sourceentity.getCapability(CatastropheModModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+						capability.spear_pin_point_strike_cooldown = _setval;
+						capability.syncPlayerVariables(sourceentity);
+					});
+				}
+			});
+			if (sourceentity.isSprinting() && sourceentity.onGround()) {
+				if ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).is(ItemTags.create(new ResourceLocation("catastrophe_mod:spears")))) {
+					if (world instanceof Level _level) {
+						if (!_level.isClientSide()) {
+							_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("catastrophe_mod:spear_pin_point")), SoundSource.PLAYERS, 1, 1);
+						} else {
+							_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("catastrophe_mod:spear_pin_point")), SoundSource.PLAYERS, 1, 1, false);
+						}
+					}
+					for (int index0 = 0; index0 < 5; index0++) {
+						pin_point_distance = pin_point_distance + 1;
+						{
+							final Vec3 _center = new Vec3(
+									(sourceentity.level()
+											.clip(new ClipContext(sourceentity.getEyePosition(1f), sourceentity.getEyePosition(1f).add(sourceentity.getViewVector(1f).scale(pin_point_distance)), ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE,
+													sourceentity))
+											.getBlockPos().getX()),
+									(sourceentity.level()
+											.clip(new ClipContext(sourceentity.getEyePosition(1f), sourceentity.getEyePosition(1f).add(sourceentity.getViewVector(1f).scale(pin_point_distance)), ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE,
+													sourceentity))
+											.getBlockPos().getY()),
+									(sourceentity.level().clip(new ClipContext(sourceentity.getEyePosition(1f), sourceentity.getEyePosition(1f).add(sourceentity.getViewVector(1f).scale(pin_point_distance)), ClipContext.Block.OUTLINE,
+											ClipContext.Fluid.NONE, sourceentity)).getBlockPos().getZ()));
+							List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(1 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
+							for (Entity entityiterator : _entfound) {
+								if (entityiterator instanceof LivingEntity) {
+									if (!(sourceentity == entityiterator)) {
+										LivingHurtEvent event2 = (LivingHurtEvent) event;
+										damage = amount;
+										event2.setAmount((float) damage);
+										if (EnchantmentHelper.getItemEnchantmentLevel(CatastropheModModEnchantments.IMPALEMENT.get(), (sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)) != 0) {
+										} else {
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
