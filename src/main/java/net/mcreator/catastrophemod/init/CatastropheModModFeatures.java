b1dbc2ccc87cf1e5e80d2d9d9c364d96281441aa
emanu <emanu@DESKTOP-8RA3B9A>
2024-07-01 04:25:12 +0200

    MCreator Merge commit '45328943c704d9ca351c98ae1b49c4b01fb9bed8' into fa2bce508089cc0fad1c57050c60195ae8dab585

--- src/main/java/net/mcreator/catastrophemod/init/CatastropheModModFeatures.java
+++ src/main/java/net/mcreator/catastrophemod/init/CatastropheModModFeatures.java
@@ -11,6 +11,7 @@ import net.minecraftforge.fml.common.Mod;
 
 import net.minecraft.world.level.levelgen.feature.Feature;
 
+import net.mcreator.catastrophemod.world.features.ManaRosePlantFeatureFeature;
 import net.mcreator.catastrophemod.CatastropheModMod;
 
 @Mod.EventBusSubscriber
